0959217019c213694f3bb0aeaaca29fdbf14d13a
lmoroco <lmorocoramos@gmail.com>
2023-03-11 12:08:04 -0500

    feat: :art: add scheduling for send mails OK

--- src/modules/services/mail.ts
+++ src/modules/services/mail.ts
@@ -1,14 +1,39 @@
 import { transporter } from '../../config/mailer';
+import schedule from 'node-schedule';
+import { mailInstance } from '../../utils/lib';
 
-export const sendTheNotify = async (
-  _to: string /*, info: [string, number]*/
-) => {
+export const sendTheNotify = async (_to: string, info: string) => {
   const nodemon = transporter();
   await nodemon.sendMail({
     from: process.env.MAILER_SOURCE,
     to: _to,
     subject: `Tini Notify`,
-    html: `<b>Hello from tinyStore<b>`,
+    html: info,
   });
   console.log('SENT');
+};
+
+export const initSchedulerForMail = () => {
+  schedule.scheduleJob('mailJob', '*/10 * * * * *', () => {
+    sendTheMails();
+  });
+};
+
+export const sendTheMails = async () => {
+  const mailsForSend = await mailInstance.findMany();
+  if (!mailsForSend || mailsForSend.length === 0) {
+    console.log('empty');
+    return;
+  }
+
+  console.log(mailsForSend);
+  for (const item of mailsForSend) {
+    sendTheNotify(item.recipient, item.body);
+
+    await mailInstance.delete({
+      where: {
+        id: item.id,
+      },
+    });
+  }
 };
